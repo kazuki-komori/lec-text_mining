ef4cd3022abbd19569dd7cf58c55c19b7a94caea
kazuki-komori <komokazu.nankai@gmail.com>
2021-11-28 12:57:34 +0000

    update: LDAVis

--- report-9/kadai.R
+++ report-9/kadai.R
@@ -4,17 +4,12 @@ library(readr)
 data.livedoor <- read_csv("./data/livedoor.csv", locale = locale(encoding = "SHIFT-JIS"))
 
 data.lda <- data.frame(rbind(data.livedoor[1:500, ], data.livedoor[1501:2000, ], data.livedoor[2001:2500, ], data.livedoor[3501:4000, ]))
+data.lda <- data.lda[, colnames(data.lda) != "OTHERS"]
 data.lda <- data.lda[, -1]
-data.lda[,1]
-sum(data.lda[colSums(data.lda) == 0, ])
-help(grep)
-head(data.livedoor)
-
+data.lda <- data.lda[, colSums(data.lda) != 0]
 
 # LDA実行
 library(topicmodels)
-
-data.lda <- data.livedoor[, -1]
 res.lda <- LDA(data.lda, method = "Gibbs", k = 4)
 
 # トピック数の確認
@@ -24,38 +19,16 @@ terms(res.lda, 10)
 terms <- posterior(res.lda)$term
 
 
-
-
 library(LDAvis)
-buildVisdata1 <- function(corpus, fit, doc_term) {
-  phi <- posterior(fit)$terms %>% as.matrix
-  theta <- posterior(fit)$topics %>% as.matrix
-  vocab <- colnames(phi)
-  doc_length <- vector()
-  for (i in 1:length(corpus)) {
-    temp <- paste(corpus[[i]]$content, collapse = ' ')
-    doc_length <- c(doc_length, stri_count(temp, regex = '\\S+'))
-  }
-  term.frequency <- colSums(as.matrix(doc_term))
-  params <- list(phi = phi,
-                 theta = theta,
-                 doc.length = doc_length,
-                 vocab = vocab,
-                 term.frequency = term.frequency)
-  return(params)
-}
+library(servr)
 
-VisualizeModel <- function(fit, doc_term, dir) {
-  params <- buildVisdata1(fit, doc_term)
-  json <- createJSON(phi = params$phi,
-                     theta = params$theta,
-                     doc.length = params$doc.length,
-                     vocab = params$vocab,
-                     term.frequency = params$term.frequency,
-                     mds.method = svd_tsne
-  )
-  serVis(json, out.dir = dir, open.browser = TRUE)
-}
-help(createJSON, package = "LDAvis")
+options(encoding = "UTF-8")
+lda.phi <- as.matrix(posterior(res.lda)$terms) # 
+lda.theta <- as.matrix(posterior(res.lda)$topics)
+lda.vocab <- colnames(lda.phi)
+lda.doc <- rowSums(data.lda)
+lda.term <- colSums(data.lda)
+lda.json <- createJSON(phi = lda.phi, theta = lda.theta, vocab = lda.vocab, term.frequency = lda.term, doc.length = lda.doc)
 
-VisualizeModel(res.lda, data.lda, "./opt")
+serVis(lda.json)
+
