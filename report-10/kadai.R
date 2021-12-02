152940ffc0f6dbd6d2ba0ca095e4bbeb34bc7241
kazuki-komori <komokazu.nankai@gmail.com>
2021-12-02 05:26:10 +0000

    update: report-10

--- report-10/kadai.R
+++ report-10/kadai.R
@@ -4,8 +4,11 @@ getwd()
 mei_akuta <- read.csv("./中間レポート/data/mei_akuta.csv", fileEncoding = "SHIFT-JIS") 
 mei_dazai <- read.csv("./中間レポート/data/mei_dazai.csv", fileEncoding = "SHIFT-JIS") 
 
+mb_akuta <- read.csv("./中間レポート/data/mb_akuta.csv") 
+mb_dazai <- read.csv("./中間レポート/data/mb_dazai.csv")
 
 
+# JSD の算出関数
 jsdist<-function(x){
   x<-x+1e-006 #ゼロである値の対数演算のため
   x<-(x/apply(x,1,sum))
@@ -16,11 +19,10 @@ jsdist<-function(x){
   })
 }
 
-
-
-
+# タイトル修正
 titles <- c("a-鼻", "a-地獄変", "a-蛙", "a-貝殻", "a-枯野抄", "a-煙管", "a-女", "a-羅生門", "a-沙羅の花", "a-トロッコ", "d-兄たち", "d-朝", "d-あさましきもの", "d-走れメロス", "d-キリギリス", "d-人間失格", "d-女の決闘", "d-惜別", "d-創世記", "d-嘘")
 
+# 名詞データの分析
 data.mei <- merge(mei_akuta, mei_dazai, all = T, ID = "Word1")
 data.mei[is.na(data.mei)] <- 0
 data.mei <- data.mei[-1, ]
@@ -28,13 +30,23 @@ rownames(data.mei) <- data.mei$Word1
 data.mei$Word1 <- NULL
 colnames(data.mei) <- titles
 
-length(colnames(data.mei))
-length(titles)
-
+# クラスター描画
 dist.mei <- jsdist(t(data.mei))
-View(dist.mei)
 hc.mei <- hclust(as.dist(dist.mei), "ward.D2")
 plot(hc.mei, hang = -1, main = "太宰治と芥川龍之介の作品 JSDクラスター")
+
+# 助詞データの分析
+data.mb <- merge(mb_akuta, mb_dazai, all = T, ID = "Word1")
+data.mb[is.na(data.mb)] <- 0
+data.mb <- data.mb[-1, ]
+rownames(data.mb) <- data.mb$Word1
+data.mb$Word1 <- NULL
+colnames(data.mb) <- titles
+
+# クラスター描画
+dist.mb <- jsdist(t(data.mb))
+hc.mb <- hclust(as.dist(dist.mb), "ward.D2")
+plot(hc.mb, hang = -1, main = "太宰治と芥川龍之介の作品 JSDクラスター")
 
 
 
